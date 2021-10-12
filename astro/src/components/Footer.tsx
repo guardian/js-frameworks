a2c004755ef03de7e30c0069b03fcbba8e6f0b65
Max Duval <max.duval@theguardian.com>
2021-10-12 14:37:16 +0100

    extract Footer

---
+++ astro/src/components/Footer.tsx
@@ -0,0 +1,8 @@
+import { h } from 'preact';
+import { footer } from '../styles/main.css';
+
+export const Footer = () => (
+  <footer className={footer}>
+    (This footer can be imported from Source)
+  </footer>
+);
